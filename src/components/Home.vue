173801269eaa88cf40a28cc338178c5eb4254f89
Marko <29685366+MarkoTON@users.noreply.github.com>
2020-12-26 14:01:29 +0100

    Adding all of Filter options

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,12 +4,17 @@
       <h1 class="text-center border-bottom">FACTORY WORLD WIDE</h1>
 
       <div class="col-12 mb-2">
-        <div class="d-inline-block">
-          <!-- <select class="form-control" v-model="pickCountry" @change="selectCountry">
-            <option v-for="(selectCountry,index) in info" v-bind:key="index" v-bind:value="selectCountry.country">{{selectCountry.country}}</option>
-          </select> -->
-          <input type="text" class="form-control" v-model="searchName" placeholder="Name">
-          <!-- <input type="text" class="form-control" v-model="searchTerm" placeholder="Balance"> -->
+        <div class="d-flex">
+          <input type="text" class="form-control mr-1" v-model="searchName" placeholder="Name exp: Allyson">
+          <input type="text" class="form-control mr-1" v-model="searchBalance" placeholder="Balance exp: 2,972.88">
+          <select class="form-control mr-1" v-model="isActive" @change="statusOfUser">
+            <option v-bind:value="'all'">Bouth</option>
+            <option v-bind:value="true" selected>Active</option>
+            <option v-bind:value="false">Not Active</option>
+          </select>
+          <input type="text" class="form-control mr-1" v-model="searchState" placeholder="Reg. exp: 2014-02-22T12:35:59">
+          <input type="text" class="form-control mr-1" v-model="searchState" placeholder="State exp: Colorado">
+          <input type="text" class="form-control" v-model="searchCountry" placeholder="State exp: Cyprus">
         </div>
       </div>
       <div class="col-12">
@@ -64,31 +69,61 @@ export default {
       countryState:[],
       stateUsers:[],
       extUserData:[],
-      pickCountry: "Chad",
       searchName:'',
+      searchBalance:'',
+      searchState:'',
+      searchCountry:'',
+      isActive: true,
       pickValue:null
     }
   },
   computed: {
     filteredUser(){
-      return this.stateUsers.filter(name => {
-        return name.fullName.toLowerCase().match(this.searchName)
-      })
+      var pickSearch = this.searchOption();
+
+      if(pickSearch.option == 'fullName'){
+        return this.stateUsers.filter((name) => {
+          return name.fullName.match(pickSearch.name)
+        });
+      } else if(pickSearch.option == 'balance'){
+        return this.stateUsers.filter((name) => {
+          return name.balance.match(pickSearch.name)
+        });
+      } else if(pickSearch.option == 'state'){
+        return this.stateUsers.filter((name) => {
+          return name.name.match(pickSearch.name)
+        });
+      } else if(pickSearch.option == 'country'){
+        return this.stateUsers.filter((name) => {
+          return name.country.match(pickSearch.name)
+        });
+      } else if(pickSearch.option == 'isActive'){
+        console.log(pickSearch.name);
+        if(pickSearch.name == 'all'){
+          return this.stateUsers.filter((name) => {
+            return name
+          });
+        } else {
+          return this.stateUsers.filter((name) => {
+            return name.isActive == pickSearch.name
+          });
+        }
+      }
     }
   },
   methods:{
-    selectCountry(){
+    statusOfUser(){
+      console.log(this.isActive);
+      
+    },
+    sortingJSON(){
       this.info.forEach(element => {
-        // if(this.pickCountry == element.country){
-          // Reset all countrys
-          // this.countryState = [];
           for(let i = 0; i < element.state.length; i++){
             let metallica = element.state[i];
             metallica.country = element.country;
             // Add all country is one array
             this.countryState.push(metallica);
           }
-        // };
       });
       // Reset all users
       this.stateUsers = [];
@@ -102,14 +137,49 @@ export default {
           this.stateUsers.push(nirvana)
         }
       });
+      // console.log(this.stateUsers)
     },
     getData() {
       return axios.get('https://fww-demo.herokuapp.com/').then(response => (this.info = response.data))
+    },
+    searchOption(){
+      if(this.searchName != ''){
+        return {
+          name: this.searchName,
+          option: 'fullName'
+        };
+      } else if(this.searchBalance != ''){
+        return {
+          name: this.searchBalance,
+          option: 'balance'
+        };
+      } else if(this.searchState != ''){
+        return {
+          name: this.searchState,
+          option: 'state'
+        };
+      } else if(this.searchCountry != ''){
+        return {
+          name: this.searchCountry,
+          option: 'country'
+        };
+      } else if(this.isActive == 'all' || this.isActive == true || this.isActive == false){
+        return {
+          name: this.isActive,
+          option: 'isActive'
+        };
+      } 
+      // else {
+      //   return {
+      //     name: this.searchName,
+      //     option: 'fullName'
+      //   };
+      // }
     }
   },
   created () {
     this.getData().then((result) => {
-      this.selectCountry()
+      this.sortingJSON()
     })
   },
   beforeCreate() {
