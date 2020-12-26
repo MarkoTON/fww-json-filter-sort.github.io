4d6f5defcf4445613b69a39c95e32bd290b25563
Marko <29685366+MarkoTON@users.noreply.github.com>
2020-12-26 17:39:31 +0100

    add font awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
